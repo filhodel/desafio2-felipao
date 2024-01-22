b45239bf4250d88cdc673b804de79623c6b38f31
filhodel <hellyson2202@gmail.com>
2024-01-22 15:48:13 -0400

    exercício feito

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // Calculadora de partidas Rankeadas
-let Calcula 
+
+calcularNivel (88, 30);
+
+function calcularNivel (vitorias, derrotas){
+
+let saldoVitorias = vitorias - derrotas;
+
+let nivel;
+if (saldoVitorias <= 10) {
+    nivel = "Ferro";
+} else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+    nivel = "Bronze";
+} else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+    nivel = "Prata";
+} else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+    nivel = "Ouro";
+} else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+    nivel = "Diamante";
+} else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
+
+console.log("O herói tem saldo de " + saldoVitorias + " e está no nível " + nivel);
+}
